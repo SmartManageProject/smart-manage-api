6fbb51e09382e51618add577e5cae9fd0b73b8bc
Ismael Abashi <ismaelabashi@hotmail.com>
2023-09-30 14:38:56 -0300

    feat: Start server with express

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
