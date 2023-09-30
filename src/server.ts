01114f3ec1cba79f544eeb0329a8f12510b50d7f
Ismael Abashi <ismaelabashi@hotmail.com>
2023-09-30 17:15:46 -0300

    feat: DB connection

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import "dotenv/config";
+import "reflect-metadata";
+
+import { AppDataSource } from "./database";
 
 import express from "express";
 
@@ -9,3 +12,11 @@ const app = express();
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
