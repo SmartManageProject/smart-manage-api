00e8635c0ea305ddabd259dd07cb69a00830b9ef
Ismael Abashi <ismaelabashi@hotmail.com>
2023-10-01 00:53:11 -0300

    feat: Create auth middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "./app/controllers/AuthController";
 import { UserController } from "./app/controllers/UserController";
+import authMiddleware from "./app/middlewares/authMiddleware";
 
 const routes = Router();
 
 routes.post("/users", new UserController().create);
+
+routes.use(authMiddleware);
+
 routes.post("/authenticate", new AuthController().authenticate);
 
 export default routes;
