fb4af7fe33183daa8b681fa1e03e374847b9d429
Ismael Abashi <ismaelabashi@hotmail.com>
2023-10-01 14:09:55 -0300

    feat: Create docs with Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import "reflect-metadata";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import routes from "./routes";
+
+import swaggerDoc from "./swagger.json";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
