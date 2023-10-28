226fab43e0b8dc4b9c569598a2d3baacfac8a250
Ismael Laet <ismaelabashi@hotmail.com>
2023-10-28 18:23:58 -0300

    feat: Create websocket server

---
+++ src/app/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import swaggerUi from "swagger-ui-express";
+
+import "../database";
+import routes from "../routes";
+import swaggerDoc from "../swagger.json";
+
+const app = express();
+
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use(routes);
+
+export { server, io };
